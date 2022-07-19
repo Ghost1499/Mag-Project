8d1f942d629952eedbbbf0d4a9ce38751d758d57
Ghost1499 <zgstv@yandex.ru>
2022-07-20 01:29:29 +0300

    Функция propose_regions заменена на класс RegionProposer

--- modules/regions.py
+++ modules/regions.py
@@ -1,9 +1,12 @@
 import typing
 from abc import ABCMeta, abstractmethod
-from typing import Tuple, Optional
+from typing import Tuple, Optional, List
 
 import cv2
 import numpy as np
+
+from modules.EdgeMapCreator import IEdgeMapCreator
+from modules.utils import image2gray, check_image, is_RGB_format, is_grayscale
 
 rsort_keys = {"area": 0, "sum_area_ratio": 1, "border_sum_area_ratio": 2, "box_border_ratio": 3}
 
@@ -28,6 +31,16 @@ def _image_region_sum(integral, y, x, height, width):
 
 
 def get_boxes_configurations(min_box_area, max_box_area, n_box_side_steps, min_sides_ratio):
+    """
+    Расчитывает конфигурции боксов для функции предложения регионов
+
+    :param min_box_area: минимальная площадь
+    :param max_box_area: максимальная площадь
+    :param n_box_side_steps: количество шагов поиска по масштабу
+    :param min_sides_ratio: минимальное соотношение сторон
+    :rtype: List[Tuple[int,int]]
+    :return: Список конфигураций боксов. Конфигурация представляет собой кортеж вида: (ширина бокса, высота бокса)
+    """
     min_box_side = np.sqrt(min_box_area)
     max_box_side = np.sqrt(max_box_area)
     if min_box_side == max_box_side:
@@ -61,17 +74,118 @@ def get_boxes_configurations(min_box_area, max_box_area, n_box_side_steps, min_s
     return configurations
 
 
-def propose_regions(img: np.ndarray, threshold: int, configurations: typing.List, alpha: float, min_box_area: int,
+def make_score(box_sum, border_sum, box_area, border_area):
+    score = [box_sum, box_sum / box_area, border_sum / border_area,
+             box_sum / box_area * (1 - border_sum / border_area)]
+    return score
+
+
+class RegionsProposer:
+    @property
+    def image_width(self):
+        return self._image.shape[1]
+
+    @property
+    def image_height(self):
+        return self._image.shape[0]
+
+    def __init__(self, image, edge_map_creator: IEdgeMapCreator, alpha: float, border_length, min_box_area,
+                 min_box_ratio, max_border_ratio, make_score_func=make_score):
+        """
+
+        :param alpha: расчет смещений боксов по X и по Y c перекрытием overloapratio=alpha.
+         Допустимые значения [0,1), где 0 - перекрытие отсутствует.
+        :param edge_map_creator:
+        """
+        self._init_image(image)
+        self._edge_map_creator = edge_map_creator
+        self.alpha = alpha
+        self.border_length = border_length
+        self.min_box_area = min_box_area
+        self.min_box_ratio = min_box_ratio
+        self.max_border_ratio = max_border_ratio
+        self.make_score = make_score_func
+        self._boxes = []
+        self._scores = []
+
+    def _init_image(self, image):
+        check_image(image)
+        if is_RGB_format(image):
+            image = self._to_gray(image)
+        if not is_grayscale(image):
+            raise Exception("Изображение не преобразовано к серому")
+        self._image = image
+
+    def _to_gray(self, img):
+        return image2gray(img)
+
+    def find_regions(self, configurations: typing.List):
+        """
+        Формирование предложений областей на основе карт перепадов(edge maps)
+
+        :param configurations: конфигурации боксов поиска предложений регионов
+        """
+        x_edge_map = self._edge_map_creator.make_x(self._image)
+        y_edge_map = self._edge_map_creator.make_y(self._image)
+        integral_x, _, _ = cv2.integral3(x_edge_map)
+        integral_y, _, _ = cv2.integral3(y_edge_map)
+
+        for conf_number, (box_width, box_height) in enumerate(configurations):
+            step_x = np.floor(box_width * (1 - self.alpha) / (1 + self.alpha))
+            step_y = np.floor(box_height * (1 - self.alpha) / (1 + self.alpha))
+            X, Y = self._make_locations(box_width=box_width, box_height=box_height, step_x=step_x, step_y=step_y)
+            self._boxes_by_configuration(integral_x, X, Y, box_width=box_width, box_height=box_height)
+            X, Y = self._make_locations(box_width=box_height, box_height=box_width, step_x=step_y, step_y=step_x)
+            self._boxes_by_configuration(integral_y, X, Y, box_width=box_height, box_height=box_height)
+        return self._boxes, self._scores
+
+    def _make_locations(self, box_width, box_height, step_x, step_y):
+        X = np.floor(np.arange(self.border_length, self.image_width - box_width - self.border_length, step_x)).astype(
+            'uint32')
+        Y = np.floor(np.arange(self.border_length, self.image_height - box_height - self.border_length, step_y)).astype(
+            "uint32")
+        return X, Y
+
+    def _boxes_by_configuration(self, img_integral, X, Y, box_width, box_height):
+        box_area = box_width * box_height
+        border_area = (box_width + 2 * self.border_length) * (box_height + 2 * self.border_length) - box_area
+        for x in X:
+            for y in Y:
+                box_sum = self._box_sum(img_integral, y, x, box_height, box_width)
+                border_sum = self._border_sum(img_integral, y, x, box_height, box_width, box_sum)
+                if self._check_region(box_sum, border_sum, box_area, border_area):
+                    box = [y, x, box_height, box_width]
+                    score = self.make_score(box_sum, border_sum, box_area, border_area)
+                    self._boxes.append(box)
+                    self._scores.append(score)
+
+    def _box_sum(self, img_integral_sum, y, x, height, width):
+        return _image_region_sum(img_integral_sum, y, x, height, width)
+
+    def _border_sum(self, img_integral_sum, y, x, height, width, box_sum):
+        border_sum = _image_region_sum(img_integral_sum, y - self.border_length, x - self, height + self,
+                                       width + self)
+        border_sum -= box_sum
+        return border_sum
+
+    def _check_region(self, box_sum, border_sum, box_area, border_area):
+        # if min_box_area < box_area_final < max_box_area:  # ???!!!
+        if box_sum / box_area > self.min_box_ratio and border_sum / border_area < self.max_border_ratio:
+            # в боксе есть крупные объекты и вдоль границ бокса мало объектов
+            return True
+        return False
+
+
+def propose_regions(image: np.ndarray, threshold: int, configurations: typing.List, alpha: float, min_box_area: int,
                     max_box_area: int,
                     delta: int, min_box_ratio: float,
                     max_border_ratio: float, rsort_key: str) -> Tuple[
-    Optional[np.ndarray], Optional[np.ndarray], int,Optional[typing.List]]:
+    Optional[np.ndarray], Optional[np.ndarray], int, Optional[typing.List]]:
     """
-    :param img: изображение
+    :param image: изображение
     :param threshold: порог для бинаризации внутри edge_boxes
-    :param alpha: расчет смещений боксов по X и по Y c перекрытием overloapratio=alpha. Допустимые значения [0,1), где 0 перекрытие отсутствует.
-    :param min_box_area: минимальная площадь
-    :param max_box_area: максимальная площадь
+    :param alpha: расчет смещений боксов по X и по Y c перекрытием overloapratio=alpha. Допустимые значения [0,1), где 0 - перекрытие отсутствует.
+
     :param min_sides_ratio: минимальное соотношение сторон
     :param n_box_side_steps: количество шагов поиска по масштабу
     :param delta: сдвиг окна
@@ -90,18 +204,18 @@ def propose_regions(img: np.ndarray, threshold: int, configurations: typing.List
     # #beta  = 0.75; # for each bbox i, suppress all surrounded bbox j where j>i and overlap
     # #ratio is larger than overlapThreshold (beta)
     # min_box_area=1000; max_box_area=700000; #площади региона
-    if img is None:
+    if image is None:
         raise ValueError("Изображение None")
     if alpha < 0 or alpha >= 1:
         raise ValueError(f"Параметр alpha={alpha} находится вне допустимого диапазона", alpha)
 
     validation_data = []
-    img_width = img.shape[1]
-    img_height = img.shape[0]
+    img_width = image.shape[1]
+    img_height = image.shape[0]
     # параметры поиска областей
 
-    img = img.astype('uint8')
-    edge_map_x, edge_map_y,cur_valid_data = edge_map(img, threshold)  # контурный анализ и обработка изображений
+    image = image.astype('uint8')
+    edge_map_x, edge_map_y, cur_valid_data = edge_map(image, threshold)  # контурный анализ и обработка изображений
     if cur_valid_data:
         validation_data.extend(cur_valid_data)
 
@@ -113,22 +227,22 @@ def propose_regions(img: np.ndarray, threshold: int, configurations: typing.List
     validation_data.append((integral_edge_map_y, "integral edge map y", None))
     # Организация начального поиска областей
     boxes_count = 0
-    step_x = np.zeros(len(configurations), np.float32)
-    step_y = np.zeros(len(configurations), np.float32)
+    steps_x = np.zeros(len(configurations), np.float32)
+    steps_y = np.zeros(len(configurations), np.float32)
     boxes = []  # генерируемые расположения и конфигурации
     scores = []
     for conf_number, (box_width, box_height) in enumerate(configurations):  # поиск по масштабу
         # расчет смещений боксов фиксированной конфигурации box_width, box_height по X и по Y c перекрытием
         # overloapratio=alpha
-        step_x[conf_number] = np.floor(box_width * (1 - alpha) / (1 + alpha))
-        step_y[conf_number] = np.floor(box_height * (1 - alpha) / (1 + alpha))
-        X_barcode_h = np.floor(np.arange(delta, img_width - box_width - delta, step_x[conf_number])).astype(
+        steps_x[conf_number] = np.floor(box_width * (1 - alpha) / (1 + alpha))
+        steps_y[conf_number] = np.floor(box_height * (1 - alpha) / (1 + alpha))
+        X_barcode_h = np.floor(np.arange(delta, img_width - box_width - delta, steps_x[conf_number])).astype(
             'uint32')
-        X_barcode_v = np.floor(np.arange(delta, img_width - box_height - delta, step_y[conf_number])).astype(
+        X_barcode_v = np.floor(np.arange(delta, img_width - box_height - delta, steps_y[conf_number])).astype(
             "uint32")
-        Y_barcode_h = np.floor(np.arange(delta, img_height - box_height - delta, step_y[conf_number])).astype(
+        Y_barcode_h = np.floor(np.arange(delta, img_height - box_height - delta, steps_y[conf_number])).astype(
             "uint32")
-        Y_barcode_v = np.floor(np.arange(delta, img_height - box_width - delta, step_x[conf_number])).astype(
+        Y_barcode_v = np.floor(np.arange(delta, img_height - box_width - delta, steps_x[conf_number])).astype(
             "uint32")
         XY_directions = [(X_barcode_h, Y_barcode_h, box_width, box_height, integral_edge_map_x),
                          (X_barcode_v, Y_barcode_v, box_height, box_width, integral_edge_map_y)]
@@ -152,7 +266,7 @@ def propose_regions(img: np.ndarray, threshold: int, configurations: typing.List
                             boxes.append(box)
                             boxes_count += 1
     if boxes_count == 0:
-        result = None, None, boxes_count,validation_data
+        result = None, None, boxes_count, validation_data
     else:
         boxes = np.array(boxes, np.uint32)
         scores = np.array(scores)
@@ -165,63 +279,13 @@ def propose_regions(img: np.ndarray, threshold: int, configurations: typing.List
     return result
 
 
-
-
-
-def edge_map(img, threshold):
-    validation_data = []
-    cmap = "img"
-    img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    validation_data.append((img_gray, 'img', cmap))
-
-    grad_x = cv2.Sobel(img_gray, ddepth=cv2.CV_64F, dx=1, dy=0, ksize=3)
-    grad_x = np.absolute(grad_x)
-    kernel_recth = cv2.getStructuringElement(cv2.MORPH_RECT, (10, 2))
-    grad_x_closed_rect = cv2.morphologyEx(grad_x, cv2.MORPH_CLOSE, kernel_recth)
-
-    grad_y = cv2.Sobel(img_gray, ddepth=cv2.CV_64F, dx=0, dy=1, ksize=3)
-    grad_y = np.absolute(grad_y)
-    kernel_recthv = np.transpose(kernel_recth, axes=(1, 0))
-    grad_y_closed_rect = cv2.morphologyEx(grad_y, cv2.MORPH_CLOSE, kernel_recthv)
-
-    validation_data.append((grad_x, 'grad x', cmap))
-    validation_data.append((grad_y, 'grad y', cmap))
-    validation_data.append((grad_x_closed_rect, "grad x closed by rectangle 2x16", cmap))
-    validation_data.append((grad_y_closed_rect, "grad y closed by rectangle 16x2", cmap))
-
-    # grad_subtract = cv2.subtract(grad_x, grad_y)  # вычитание градиентов
-    # grad_subtract = np.absolute(grad_subtract)
-
-    # kernel_round = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (3, 3))
-    # grad_subtract_bin = cv2.morphologyEx(grad_subtract_bin, cv2.MORPH_OPEN, kernel_round, iterations=1)
-
-    # grad_x_closed_rect = cv2.erode(grad_x_closed_rect, None, iterations=4)
-    # grad_x_closed_rect = cv2.dilate(grad_x_closed_rect, None, iterations=4)
-
-    kernel_square = cv2.getStructuringElement(cv2.MORPH_RECT, (8, 8))
-    grad_x_opened_square = cv2.morphologyEx(grad_x_closed_rect, cv2.MORPH_OPEN, kernel_square)
-    grad_y_opened_square = cv2.morphologyEx(grad_y_closed_rect, cv2.MORPH_OPEN, kernel_square)
-    validation_data.append((grad_x_opened_square, "grad x opening square 8x8", cmap))
-    validation_data.append((grad_y_opened_square, "grad y opening square 8x8", cmap))
-
-    # grad_x_opened_square = cv2.erode(grad_x_opened_square, None, iterations=4)
-    # grad_x_opened_square = cv2.dilate(grad_x_opened_square, None, iterations=4)
-
-    (_, grad_x_bin) = cv2.threshold(grad_x_opened_square, threshold, 255, cv2.THRESH_BINARY)
-    (_, grad_y_bin) = cv2.threshold(grad_y_opened_square, threshold, 255, cv2.THRESH_BINARY)
-    validation_data.append((grad_x_bin, "grad x binary", cmap))
-    validation_data.append((grad_y_bin, "grad y binary", cmap))
-
-    return grad_x_bin, grad_y_bin, validation_data
-
-
 def append_border(boxes: typing.Union[np.ndarray, typing.List]) -> np.ndarray:
     """
     :type boxes: typing.Union[np.ndarray, typing.List]
     :rtype: np.ndarray
     Прибавляет к боксу рамку и убирает её из списка значений кортежа
     """
-    boxes_plus_border = np.empty((boxes.shape[0],boxes.shape[1]-1),np.int32)
+    boxes_plus_border = np.empty((boxes.shape[0], boxes.shape[1] - 1), np.int32)
     for i, (y, x, height, width, border) in enumerate(boxes):
         boxes_plus_border[i] = (y - border, x - border, height + border, width + border)
     return boxes_plus_border
--- modules/utils.py
+++ modules/utils.py
@@ -2,6 +2,9 @@ import os
 import time
 from pathlib import Path
 
+import cv2 as cv
+import numpy as np
+
 
 def benchmark(func):
     def wrapper(*args, **kwargs):
@@ -10,6 +13,7 @@ def benchmark(func):
         end = time.time()
         print('[*] Время выполнения: {} секунд.'.format(end - start))
         return res
+
     return wrapper
 
 
@@ -17,19 +21,43 @@ def is_horizontal(size):
     return size[0] < size[1]
 
 
+def check_image(img: np.ndarray):
+    if img is None:
+        raise ValueError("Изображение None")
+    if not isinstance(img, np.ndarray):
+        raise TypeError(f"Тип входного аргумента {type(img)}, а не Numpy Array")
+    if 2 > img.ndim > 3:
+        raise ValueError(f"Входной аргумент img имеет неподходящее число измерений: {img.ndim}")
+    if img.ndim == 3:
+        if img.shape[2] not in (3, 4):
+            raise ValueError(f"Входной аргумент img имеет неподходящее число каналов: {img.shape[2]}")
+
+
+# noinspection PyPep8Naming
+def is_RGB_format(img: np.ndarray):
+    if img.ndim == 3 and img.shape[2] == 3:
+        return True
+    return False
+
+
+def is_grayscale(img: np.ndarray):
+    if img.ndim == 2:
+        return True
+    return False
+
+
+def image2gray(img: np.ndarray):
+    if is_RGB_format(img):
+        return cv.cvtColor(img, cv.COLOR_RGB2GRAY)
+
+
 def images_in_dir_generator(source_dir: [str, Path], result_dir: [str, Path]):
-    # if fkwargs is None:
-    #     fkwargs={}
     if type(source_dir) == str:
         source_dir = Path(source_dir)
     if type(result_dir) == str:
         result_dir = Path(result_dir)
     if not source_dir.is_dir():
         raise Exception("Тестовая директория не существует")
-    # if not callable(func):
-    #     raise ValueError(f"Аргумент {func} не является вызываемым")
-    # if not os.listdir(source_dir):
-    #     raise Exception("Тестовая директория пустая")
     if not result_dir.is_dir():
         result_dir.mkdir(parents=True)
     for root, dirs, files in os.walk(str(source_dir)):
@@ -41,4 +69,3 @@ def images_in_dir_generator(source_dir: [str, Path], result_dir: [str, Path]):
                 cur_res_dir = Path(root).relative_to(source_dir)
                 save_dir = result_dir / cur_res_dir
                 yield file_path, save_dir
-                # func(file_path, save_dir, *fargs, **fkwargs)
